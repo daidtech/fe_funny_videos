d9b8e3f36e33c0c99e9634a4b09e834b2e66aa85
daidv <dvdai63@gmail.com>
2025-02-12 22:05:25 +0700

    handle login

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,46 @@
+'use client';
 import { Button, Form, Container, Row, Col } from 'react-bootstrap';
 import { House } from 'lucide-react';
+import { useEffect, useState } from 'react';
+import { getCurrentUser, login, logout, register } from '../services/authService';
 
 const Header = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [currentUser, setCurrentUser] = useState(null);
+
+  const fetchCurrentUser = async () => {
+    await getCurrentUser().then((res) => {
+      setCurrentUser(res);
+    })
+  }
+
+  const handleLogin = () => {
+    login(email, password).then((res) => {
+      fetchCurrentUser();
+    }).catch((err) => {});
+  }
+
+  const handleRegister = () => {
+    register(email, password).then((res) => {
+      console.log(res);
+    }).catch((err) => {});
+  }
+  const handleLogout = () => {
+    logout().then((res) => {
+      console.log(res);
+      setCurrentUser(null);
+    }).catch((err) => {});
+  }
+
+  useEffect(() => {
+    fetchCurrentUser();
+  }, []);
+
+  useEffect(() => {
+    console.log('currentUser', currentUser)
+  }, [currentUser]);
+
   return (
     <header className="border-bottom">
       <Container className="py-4">
@@ -10,12 +49,36 @@ const Header = () => {
             <House className="h-6 w-6" />
             <h1 className="h4 font-weight-bold mb-0">Funny Movies</h1>
           </Col>
-          <Col md={8} className="d-flex align-items-center justify-content-end gap-4">
-            <Form.Control type="email" placeholder="email" className="w-25" />
-            <Form.Control type="password" placeholder="password" className="w-25" />
-            <Button variant="primary">Login</Button>
-            <Button variant="secondary">Register</Button>
-          </Col>
+            <Col md={8} className="d-flex align-items-center justify-content-end gap-4">
+              <>
+                {
+                  currentUser?.id ? (
+                    <>
+                      <h4 className='mb-0'>Welcome {currentUser?.email}</h4>
+                      <Button variant="primary">ShareVideo</Button>
+                      <Button variant="danger" onClick={() => handleLogout()}>Logout</Button>
+                    </>
+                  ) : (
+                    <>
+                      <Form.Control
+                        type="email"
+                        placeholder="email"
+                        className="w-25"
+                        onChange={(e) => setEmail(e.target.value)}
+                      />
+                      <Form.Control
+                        type="password"
+                        placeholder="password"
+                        className="w-25"
+                        onChange={(e) => setPassword(e.target.value)}
+                      />
+                      <Button variant="primary" onClick={() => handleLogin()}>Login</Button>
+                      <Button variant="secondary" onClick={() => handleRegister()}>Register</Button>
+                    </>
+                  )
+                }
+              </>
+            </Col>
         </Row>
       </Container>
     </header>
---
+++ src/services/authService.ts
@@ -0,0 +1,58 @@
+import { httpClientWithoutVersion } from './httpClient';
+import Cookies from 'js-cookie';
+
+export const getCurrentUser = async () => {
+  try {
+    const token = Cookies.get('token');
+    if(token) {
+      const response = await httpClientWithoutVersion.get('/users/current_user', {
+        headers: {
+          Authorization: 'Bearer ' + token
+        }
+      });
+      return response.data;
+    }
+    return ''
+  } catch (error) {
+    throw new Error('Lỗi xảy ra');
+  }
+};
+
+export const login = async (email: string, password: string) => {
+  try {
+    const response = await httpClientWithoutVersion.post('/users/sign_in', { user: {email, password} });
+    const { token } = response.data;
+    Cookies.set('token', token);
+    return response.data;
+  } catch (error: any) {
+    if(error.response.status==401) {
+      throw new Error('Email hoặc mật khẩu không đúng');
+    } else {
+      throw new Error("Hệ thống quá tải, vui lòng thử lại sau");
+    }
+  }
+};
+
+export const register = async (email: string, password: string) => {
+  try {
+    const response = await httpClientWithoutVersion.post('/users', { user: {email, password} });
+    return response.data;
+  } catch (error: any) {
+    throw new Error(error?.response?.data.status.error);
+  }
+};
+
+export const logout = async () => {
+  try {
+    const token = Cookies.get('token');
+    if (token) {
+      await httpClientWithoutVersion.delete('/users/sign_out', {headers: { "Authorization": `Bearer ${token}`}});
+      Cookies.remove('token');
+    } else {
+
+    }
+
+  } catch (error) {
+    throw new Error('Lôĩ xảy ra');
+  }
+};
