e2eb4b5368204792e156a6c45b1552e5f01cfa88
daidv <dvdai63@gmail.com>
2025-02-13 06:42:58 +0700

    Add react toastify

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { ToastContainer } from "react-toastify";
 import FunnyMovies from "../components/ListMovie";
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
     <div>
       <main>
         <FunnyMovies />
+        <ToastContainer limit={5} position={'bottom-right'}/>
       </main>
     </div>
   );
